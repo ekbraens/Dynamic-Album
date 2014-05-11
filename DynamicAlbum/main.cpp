221a340c2e367a73162a177dcde0de8f1f0ec6e2
New <new@news-air.gateway.sonic.net>
2014-05-11 15:52:02 -0700

    First Commit

---
+++ DynamicAlbum/main.cpp
@@ -0,0 +1,17 @@
+//
+//  main.cpp
+//  DynamicAlbum
+//
+//  Created by New on 5/11/14.
+//  Copyright (c) 2014 New. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    std::cout << "Hey Buddy!\n";
+    return 0;
+}
+
